dee18b74d57686416dc70e568a40bddb528bc565
PaulHaze <dev@artemist.com.au>
2022-10-14 06:01:50 +1100

    feat: create header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export function header() {
+  return (
+    <div className="w-full bg-white flex max-w-lg justify-between">
+      <p>logo</p>
+      <p>switch</p>
+    </div>
+  );
+}
