89e5f9c03ec7461381b600291b4d21a8561e876c
PaulHaze <dev@artemist.com.au>
2022-10-14 06:52:29 +1100

    feat: header style fixes

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,9 +4,11 @@ import styles from './Header.module.scss';
 
 export function Header() {
   return (
-    <div className={styles.headerWrapper}>
-      <p>logo</p>
-      <p>switch</p>
+    <div className="w-full bg-white">
+      <div className={styles.headerWrapper}>
+        <p>logo</p>
+        <p>switch</p>
+      </div>
     </div>
   );
 }
