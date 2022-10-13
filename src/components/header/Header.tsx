af92b81b82c20a350d66c34791511f31e7573910
PaulHaze <dev@artemist.com.au>
2022-10-14 06:59:33 +1100

    fix: small header style changes

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
 import styles from './Header.module.scss';
 
@@ -6,7 +8,15 @@ export function Header() {
   return (
     <div className="w-full bg-white">
       <div className={styles.headerWrapper}>
-        <p>logo</p>
+        <Link href="/">
+          <Image
+            height={25}
+            width={175}
+            className="cursor-pointer"
+            src="/logo-desktop.svg"
+            alt="Frontend mentor logo"
+          />
+        </Link>
         <p>switch</p>
       </div>
     </div>
