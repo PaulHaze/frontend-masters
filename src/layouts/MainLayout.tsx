e0e387f313dcad736466eb1e7f0e183b99586e0a
PaulHaze <dev@artemist.com.au>
2022-10-16 05:59:57 +1100

    chore: tody up mainlayout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import type { ReactNode } from 'react';
 
+import { Meta } from '@Layouts';
+
 import { Header } from '@Components';
 
 import { ThemeContext } from '@/context/ThemeContext';
@@ -10,11 +12,14 @@ type MainLayoutProps = {
   children: ReactNode;
 };
 
-export function MainLayout({ meta, children }: MainLayoutProps) {
+export function MainLayout({ children }: MainLayoutProps) {
   const { darkMode } = React.useContext(ThemeContext);
   return (
     <div className={`w-full antialiased ${darkMode ? 'dark' : ''}`}>
-      {meta}
+      <Meta
+        title="FrontEnd Masters Projects"
+        description="A home for all the challenges provided by FrontEnd Masters"
+      />
       <div className="bg-white w-full dark:bg-slate-800 dark:text-offwhite ">
         <Header darkMode={darkMode} />
         <div>{children}</div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,9 @@
 import type { NextPage } from 'next';
-import { Meta, MainLayout } from '@/layouts';
+import { MainLayout } from '@/layouts';
 
 const Home: NextPage = () => {
   return (
-    <MainLayout
-      meta={
-        <Meta
-          title="FrontEnd Masters Projects"
-          description="A home for all the challenges provided by FrontEnd Masters"
-        />
-      }
-    >
+    <MainLayout>
       <div className="h-[100vh] flex flex-col items-center dark:text-offwhite">
         <h1>FrontEnd Masters Project</h1>
       </div>
