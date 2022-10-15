1fa7bea0a93d06fc085744b0a127fd755402766a
PaulHaze <dev@artemist.com.au>
2022-10-16 10:13:20 +1100

    fix: pre-tsconfig edit

--- src/features/newbie/product-card/ProductReviewCard.tsx
+++ src/features/newbie/product-card/ProductReviewCard.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
 
 export function ProductReviewCard() {
+  let testNum = 5;
+  testNum = 'tetst';
+
   return (
     <div>
       <p>ProductReviewCard</p>
+      {testNum}
     </div>
   );
 }
