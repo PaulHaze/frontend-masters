f7b40d6992fea4d18b8197f858e9e5050527c2df
PaulHaze <dev@artemist.com.au>
2022-10-16 09:20:51 +1100

    feat: thumbnail finished for review card

--- src/pages/newbie/index.tsx
+++ src/pages/newbie/index.tsx
@@ -6,25 +6,27 @@ import Link from 'next/link';
 
 import { useThemeContext } from '@/context/ThemeContext';
 
-import CardThumbLight from '@/assets/img/review-card/review-card-light.jpg';
-import CardThumbDark from '@/assets/img/review-card/review-card-dark.jpg';
+import CardThumbLight from '@/assets/img/review-card/review-card-light.png';
+import CardThumbDark from '@/assets/img/review-card/review-card-dark.png';
 
 const Newbie: NextPage = () => {
   const { darkMode } = useThemeContext();
   return (
     <MainLayout>
-      <div className="flex max-w-lg mx-auto justify-center flex-wrap gap-2 min-h-[100vh] pt-10">
-        <Link href="newbie/product-card">
-          <div className=" w-48 bb overflow-hidden cursor-pointer">
-            <div className=" hover:scale-[1.2] transition-all duration-500">
-              <Image
-                src={darkMode ? CardThumbDark : CardThumbLight}
-                alt="Thumbnail link to Review Card Component"
-                layout="fill"
-              />
+      <div className="min-h-[100vh]  pt-10">
+        <div className="flex max-w-lg mx-auto justify-center flex-wrap gap-2">
+          <Link href="newbie/product-card">
+            <div className="w-52 overflow-hidden cursor-pointer rounded shadow inline-flex">
+              <div className="hover:scale-[1.1] transition-all duration-700">
+                <Image
+                  src={darkMode ? CardThumbDark : CardThumbLight}
+                  alt="Thumbnail link to Review Card Component"
+                  layout="fill"
+                />
+              </div>
             </div>
-          </div>
-        </Link>
+          </Link>
+        </div>
       </div>
     </MainLayout>
   );
