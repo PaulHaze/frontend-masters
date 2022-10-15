4dfca66500d6d9d649d94f5a8237b18c53144bd3
PaulHaze <dev@artemist.com.au>
2022-10-16 10:17:25 +1100

    chore: remove uneeded code

--- src/features/newbie/product-card/ProductReviewCard.tsx
+++ src/features/newbie/product-card/ProductReviewCard.tsx
@@ -1,13 +1,9 @@
 import * as React from 'react';
 
 export function ProductReviewCard() {
-  let testNum = 5;
-  testNum = 'tetst';
-
   return (
     <div>
       <p>ProductReviewCard</p>
-      {testNum}
     </div>
   );
 }
