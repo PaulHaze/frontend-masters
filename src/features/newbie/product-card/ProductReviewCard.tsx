bd9d9af10de4d25502f86f109cb2c938811edf07
PaulHaze <dev@artemist.com.au>
2022-10-17 09:14:04 +1100

    feat: image placeholder in review card

--- src/features/newbie/product-card/ProductReviewCard.tsx
+++ src/features/newbie/product-card/ProductReviewCard.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
 
+// import Image from 'next/future/image';
+
 export function ProductReviewCard() {
   return (
     <div className="w-full min-h-screen bg-[#f2ebe3] flex-center px-6">
-      <div className="w-full h-[610px] bb">
+      <div className="w-full h-[610px] bb rounded-[9px] overflow-hidden">
+        <div className="image-container relative w-full h-[240px] bg-slate-600 rounded-[9px]">
+          IMAGE
+        </div>
         <p>ProductReviewCard</p>
       </div>
     </div>
