e1d3ad1144f0b2974c0980e5335afd0bbfeb0bfc
PaulHaze <dev@artemist.com.au>
2022-10-17 05:53:24 +1100

    feat: experiment with tailwind variables

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,17 @@ import { MainLayout } from '@Layouts';
 
 import Link from 'next/link';
 
+import { useThemeContext } from '@/context/ThemeContext';
+
 const Home: NextPage = () => {
+  const { darkMode } = useThemeContext();
+  const themeBg = darkMode ? 'bg-gray-50 text-black' : 'bg-gray-900 text-white';
+
   return (
     <MainLayout>
       <div className="h-[100vh] flex flex-col items-center dark:text-offwhite">
         <Link href="/newbie">
-          <a className="w-40 aspect-[3/5] bg-gray-300 rounded flex-center">
+          <a className={`w-40 aspect-[3/5] rounded flex-center ${themeBg}`}>
             Newbie Thumbnail
           </a>
         </Link>
