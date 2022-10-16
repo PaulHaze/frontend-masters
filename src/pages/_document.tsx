f1577743ca764c16f67e344817b032d7c13692ed
PaulHaze <dev@artemist.com.au>
2022-10-17 09:56:07 +1100

    fix: add custom font into head component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,14 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang={AppConfig.locale}>
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght@9..144,700&family=Montserrat:wght@400;600&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
