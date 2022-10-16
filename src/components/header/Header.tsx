3d8ae8b52aaed674f281881b065f9bba27f54a26
PaulHaze <dev@artemist.com.au>
2022-10-17 07:04:36 +1100

    feat: swap logo image on theme change

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,18 +2,22 @@ import * as React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 
+import { useTheme } from 'next-themes';
+
 import { DarkModeToggle } from './DarkModeToggle';
 
 import styles from './Header.module.scss';
 
-export type Mode = 'dark' | 'light' | 'toggle';
+// export type Mode = 'dark' | 'light' | 'toggle';
 
-export type HeaderProps = {
-  darkMode: boolean;
-};
+// export type HeaderProps = {
+//   darkMode: boolean;
+// };
 
-export function Header({ darkMode }: HeaderProps) {
-  const logoSrc = darkMode ? '/logo-desktop-dark.svg' : '/logo-desktop.svg';
+export function Header() {
+  const { theme } = useTheme();
+  const logoSrc =
+    theme === 'dark' ? '/logo-desktop-dark.svg' : '/logo-desktop.svg';
   return (
     <div className="w-full">
       <div className={styles.headerWrapper}>
