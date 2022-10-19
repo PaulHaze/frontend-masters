dda8be2d886e0f4aaee6041a910e62bf403e39ca
PaulHaze <dev@artemist.com.au>
2022-10-20 08:05:48 +1100

    fix: add card bg to card page

--- src/features/newbie/product-card/ReviewCard.tsx
+++ src/features/newbie/product-card/ReviewCard.tsx
@@ -8,51 +8,49 @@ import CardImgMobile from './assets/image-product-mobile.jpg';
 
 export function ReviewCard() {
   return (
-    <div className={styles.container}>
-      <div className={styles.cardContainer}>
-        {/* IMAGE COMPONENT */}
-        <div className={styles.imageContainer}>
-          <Image
-            src={CardImgMobile}
-            alt="Image of Chanel perfume"
-            className="overflow-hidden md:hidden object-cover"
-            fill
-          />
+    <div className={styles.cardContainer}>
+      {/* IMAGE COMPONENT */}
+      <div className={styles.imageContainer}>
+        <Image
+          src={CardImgMobile}
+          alt="Image of Chanel perfume"
+          className="overflow-hidden md:hidden object-cover"
+          fill
+        />
 
-          <Image
-            src={CardImgDesktop}
-            alt="Image of Chanel perfume"
-            className="hidden md:block overflow-hidden"
-            fill
-          />
+        <Image
+          src={CardImgDesktop}
+          alt="Image of Chanel perfume"
+          className="hidden md:block overflow-hidden"
+          fill
+        />
+      </div>
+      {/* CARD TEXT CONTENT */}
+      <div className={styles.contentContainer}>
+        <div className={styles.cardCategory}>
+          <p>PERFUME</p>
         </div>
-        {/* CARD TEXT CONTENT */}
-        <div className={styles.contentContainer}>
-          <div className={styles.cardCategory}>
-            <p>PERFUME</p>
-          </div>
-          <div className={styles.cardTitle}>
-            <h4>Gabrielle Essence Eau De Parfum</h4>
-          </div>
-          <div className={styles.cardBody}>
-            <p>
-              A floral, solar and voluptuous interpretation composed by Olivier
-              Polge, Perfumer-Creator for the House of CHANEL.
-            </p>
-          </div>
-          <div className={styles.price}>
-            <h5>$149.99</h5>
-            <p>$169.99</p>
-          </div>
-          <div className={styles.buttonContainer}>
-            <button type="button">
-              <img
-                src="/icons/review-card/CartBtnIcon.svg"
-                alt="Shopping Cart Icon"
-              />
-              <h5> Add to Cart</h5>
-            </button>
-          </div>
+        <div className={styles.cardTitle}>
+          <h4>Gabrielle Essence Eau De Parfum</h4>
+        </div>
+        <div className={styles.cardBody}>
+          <p>
+            A floral, solar and voluptuous interpretation composed by Olivier
+            Polge, Perfumer-Creator for the House of CHANEL.
+          </p>
+        </div>
+        <div className={styles.price}>
+          <h5>$149.99</h5>
+          <p>$169.99</p>
+        </div>
+        <div className={styles.buttonContainer}>
+          <button type="button">
+            <img
+              src="/icons/review-card/CartBtnIcon.svg"
+              alt="Shopping Cart Icon"
+            />
+            <h5> Add to Cart</h5>
+          </button>
         </div>
       </div>
     </div>
--- src/pages/newbie/product-card.tsx
+++ src/pages/newbie/product-card.tsx
@@ -2,13 +2,29 @@ import { MainLayout } from '@Layouts';
 
 import type { NextPage } from 'next';
 
-import { ReviewCard } from '@/features/newbie';
+import { useResponsiveSettings } from '@/hooks';
+
+import {
+  ReviewCard,
+  ReviewCardDesktop,
+  ReviewCardMobile,
+} from '@/features/newbie';
 
 const ProductCardPage: NextPage = () => {
+  const { isMobile, isDesktop } = useResponsiveSettings();
+
+  // eslint-disable-next-line no-nested-ternary
+  const cardToShow = isMobile ? (
+    <ReviewCardMobile />
+  ) : isDesktop ? (
+    <ReviewCardDesktop />
+  ) : (
+    <ReviewCard />
+  );
   return (
     <MainLayout>
-      <div className="h-[100vh] flex flex-col items-center dark:text-offwhite">
-        <ReviewCard />
+      <div className="h-[100vh] flex-center bg-[#f2ebe3] dark:text-offwhite dark:bg-[#6c7289]">
+        {cardToShow}
       </div>
     </MainLayout>
   );
