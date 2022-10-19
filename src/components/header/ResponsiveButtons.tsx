5432075635fe4c374ea6f3ed0109bd7e5cfe6571
PaulHaze <dev@artemist.com.au>
2022-10-20 08:11:00 +1100

    feat: conditional styling to responsive buttons

--- src/components/header/ResponsiveButtons.tsx
+++ src/components/header/ResponsiveButtons.tsx
@@ -1,29 +1,36 @@
 import * as React from 'react';
 
-import { useResponsiveStore } from '@/store';
+import { useResponsiveSettings } from '@/hooks';
 
 export function ResponsiveButtons() {
-  const setViewSetting = useResponsiveStore((state) => state.setViewSetting);
+  const { setViewSetting, isMobile, isDesktop, isResponsive } =
+    useResponsiveSettings();
   return (
     <div className="flex">
       <button
         type="button"
         onClick={() => setViewSetting('responsive')}
-        className="px-2 rounded border text-xs"
+        className={`px-2 rounded border text-xs ${
+          isResponsive && 'bg-gray-800 text-white'
+        }`}
       >
         Responsive
       </button>
       <button
         type="button"
         onClick={() => setViewSetting('desktop')}
-        className="px-2 rounded border text-xs"
+        className={`px-2 rounded border text-xs ${
+          isDesktop && 'bg-gray-800 text-white'
+        }`}
       >
         desktop
       </button>
       <button
         type="button"
         onClick={() => setViewSetting('mobile')}
-        className="px-2 rounded border text-xs"
+        className={`px-2 rounded border text-xs ${
+          isMobile && 'bg-gray-800 text-white'
+        }`}
       >
         mobile
       </button>
--- src/features/newbie/product-card/ReviewCardDesktop.tsx
+++ src/features/newbie/product-card/ReviewCardDesktop.tsx
@@ -1,9 +1,58 @@
 import * as React from 'react';
+import Image from 'next/future/image';
+
+import styles from './Card.module.scss';
+
+import CardImgDesktop from './assets/image-product-desktop.jpg';
+import CardImgMobile from './assets/image-product-mobile.jpg';
 
 export function ReviewCardDesktop() {
   return (
-    <div>
-      <p>ReviewCardDesktop Placeholder</p>
+    <div className={styles.cardContainer}>
+      {/* IMAGE COMPONENT */}
+      <div className={styles.imageContainer}>
+        <Image
+          src={CardImgMobile}
+          alt="Image of Chanel perfume"
+          className="overflow-hidden md:hidden object-cover"
+          fill
+        />
+
+        <Image
+          src={CardImgDesktop}
+          alt="Image of Chanel perfume"
+          className="hidden md:block overflow-hidden"
+          fill
+        />
+      </div>
+      {/* CARD TEXT CONTENT */}
+      <div className={styles.contentContainer}>
+        <div className={styles.cardCategory}>
+          <p>PERFUME</p>
+        </div>
+        <div className={styles.cardTitle}>
+          <h4>Gabrielle Essence Eau De Parfum</h4>
+        </div>
+        <div className={styles.cardBody}>
+          <p>
+            A floral, solar and voluptuous interpretation composed by Olivier
+            Polge, Perfumer-Creator for the House of CHANEL.
+          </p>
+        </div>
+        <div className={styles.price}>
+          <h5>$149.99</h5>
+          <p>$169.99</p>
+        </div>
+        <div className={styles.buttonContainer}>
+          <button type="button">
+            <img
+              src="/icons/review-card/CartBtnIcon.svg"
+              alt="Shopping Cart Icon"
+            />
+            <h5> Add to Cart</h5>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
