8c110fec0b75e1435cdd8687bb9b962856bf2977
PaulHaze <dev@artemist.com.au>
2022-10-21 07:50:52 +1100

    feat: add title and link buttons to review card

--- src/pages/newbie/product-card.tsx
+++ src/pages/newbie/product-card.tsx
@@ -23,7 +23,38 @@ const ProductCardPage: NextPage = () => {
   );
   return (
     <MainLayout>
-      <div className="h-[100vh] flex-center bg-[#f2ebe3] dark:text-offwhite dark:bg-[#6c7289]">
+      <div className="h-[100vh] flex flex-col items-center bg-[#f2ebe3] dark:text-offwhite dark:bg-[#6c7289]">
+        <div className="pt-10 mb-10 inline-flex justify-center gap-2 md:flex md:justify-between w-full max-w-[700px]">
+          <h1 className="text-xl sm:text-2xl md:text-3xl text-center font-semibold text-stone-700 dark:text-slate-900">
+            Newbie Challenge:
+          </h1>
+          <h1 className="text-xl sm:text-2xl md:text-3xl text-center font-semibold text-stone-700 dark:text-slate-900">
+            Product Review Card
+          </h1>
+        </div>
+        <div
+          className={`flex justify-between w-[360px] mb-2 ${
+            !isMobile && 'md:w-[700px]'
+          }`}
+        >
+          <div className="px-5 py-3 rounded bg-white text-stone-700 text-sm shadow cursor-pointer hover:bg-stone-600 hover:text-white dark:bg-slate-700 dark:hover:bg-slate-900 dark:text-offwhite transition-all">
+            <a
+              href="https://www.frontendmentor.io/challenges/product-preview-card-component-GO7UmttRfa"
+              target="_blank"
+              rel="noopener noreferrer"
+            ></a>
+            View Challenge
+          </div>
+          <div className="px-5 py-3 rounded bg-white text-stone-700 text-sm shadow cursor-pointer hover:bg-stone-600 hover:text-white dark:bg-slate-700 dark:hover:bg-slate-900 dark:text-offwhite transition-all">
+            <a
+              href="https://www.frontendmentor.io/challenges/product-preview-card-component-GO7UmttRfa"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              View On Github
+            </a>
+          </div>
+        </div>
         {cardToShow}
       </div>
     </MainLayout>
