4e20ae41c89c18467a0661749100a59551ccae7b
PaulHaze <dev@artemist.com.au>
2022-10-24 10:24:24 +1100

    fix: style fixes

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -11,7 +11,7 @@ export function Header() {
   const { isDarkMode } = useTheme();
   const logoSrc = isDarkMode ? '/logo-desktop-dark.svg' : '/logo-desktop.svg';
   return (
-    <header className="w-full px-1">
+    <header className="w-full px-">
       <div className="max-w-lg mx-auto flex justify-between px-2 sm:px-4 md:px-8 py-5">
         <Link href="/">
           <div className="relative mt-1 w-[150px] sm:w-[180px] sm:h-[55px]">
@@ -23,10 +23,7 @@ export function Header() {
             />
           </div>
         </Link>
-        <div
-          className="flex flex-grow justify-center relative right-10 sm:right-20
-        "
-        >
+        <div className="flex flex-grow justify-center relative right-10 sm:right-20">
           <ResponsiveButtons />
         </div>
         <DarkModeSwitch />
