250ae1bc9d69a5abaa69ccc952a6c5c70ed6b98d
PaulHaze <dev@artemist.com.au>
2022-11-10 10:28:38 +1100

    feat: add radial bg to feedback card

--- src/features/newbie/feedback-card/FeedbackCard.tsx
+++ src/features/newbie/feedback-card/FeedbackCard.tsx
@@ -1,7 +1,16 @@
+import { Overpass } from '@next/font/google';
+
+const overpass = Overpass({
+  weight: 'variable',
+  subsets: ['latin'],
+});
+
 export function FeedbackCard() {
   return (
-    <div>
-      <p>FeedbackCard Placeholder</p>
+    <div
+      className={`${overpass.className} flex flex-grow text-white h-[414px] max-w-[412px] mx-auto rounded-[30px] feedback-card-bg`}
+    >
+      <p className="text-4xl font-semibold">FeedbackCard Placeholder</p>
     </div>
   );
 }
