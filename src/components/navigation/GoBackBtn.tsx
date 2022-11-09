9f950170b092cb98dece1d0561f1de76acbb2de6
PaulHaze <dev@artemist.com.au>
2022-11-09 16:35:48 +1100

    fix: add router from new location

--- src/components/navigation/GoBackBtn.tsx
+++ src/components/navigation/GoBackBtn.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react';
+// 'use client';
 
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 import { FiChevronLeft } from 'react-icons/fi';
 
