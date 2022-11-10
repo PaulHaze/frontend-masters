a139377feff26213851e3db940a25cb31af49184
PaulHaze <dev@artemist.com.au>
2022-11-10 17:02:00 +1100

    feat: add state to handle selecting rating

--- src/features/newbie/feedback-card/Rating.tsx
+++ src/features/newbie/feedback-card/Rating.tsx
@@ -1,19 +1,27 @@
 type RatingProps = {
   rating: string;
-  selectedRating?: string;
+  selectedRating: string;
+  handleSelectRating: () => void;
 };
 
-export function Rating({ rating, selectedRating }: RatingProps) {
+export function Rating({
+  rating,
+  selectedRating,
+  handleSelectRating,
+}: RatingProps) {
   const ratingBg = selectedRating === rating ? 'bg-[#7A8898]' : 'bg-[#262F38]';
-  const ratingNumber =
+  const ratingColor =
     selectedRating === rating ? 'text-white' : 'text-white/50';
+
   return (
-    <div
+    <button
+      type="button"
+      onClick={() => handleSelectRating(rating)}
       className={`w-12 h-12 ${ratingBg} rounded-full flex-center group hover:bg-[#EA7B35] cursor-pointer`}
     >
-      <p className={`font-semibold ${ratingNumber} group-hover:text-white`}>
+      <p className={`font-semibold ${ratingColor} group-hover:text-white`}>
         {rating}
       </p>
-    </div>
+    </button>
   );
 }
