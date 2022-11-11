84a6b2a183b491299344981dd22a0c2071bc3707
PaulHaze <dev@artemist.com.au>
2022-11-12 09:11:49 +1100

    feat: add separate style module for modal

--- src/features/newbie/feedback-card/SubmittedModal.tsx
+++ src/features/newbie/feedback-card/SubmittedModal.tsx
@@ -1,7 +1,13 @@
+import styles from './Feedback.module.scss';
+
 export function SubmittedModal() {
   return (
-    <div>
-      <p>SubmittedModal Placeholder</p>
+    <div className="fixed top-0 left-0 z-20 w-screen h-screen bg-black/50 backdrop-blur-sm flex-center px-4">
+      <div className={styles.container}>
+        <div className="">
+          <p>SubmittedModal Placeholder</p>
+        </div>
+      </div>
     </div>
   );
 }
